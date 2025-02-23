0c43101601136767713287bbafdf3687cb6ed7b6
KVidyaReddy <reddyvidya1998@gmail.com>
2025-02-23 11:53:38 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,7 +6,6 @@ public class Test {
         int b=10;
         int res=a+b;
         System.out.println("the result is " +res);
-        System.out.println("In Line 9");
     }
     public static void main(String[] args)
     {
